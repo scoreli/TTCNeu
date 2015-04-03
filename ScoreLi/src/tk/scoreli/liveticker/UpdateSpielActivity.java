6ffb8b6728bc60ae518efd502b952ae0dc13fe52
Philipp Messmer <philipp.messmer@gmail.com>
2015-04-04 01:04:43 +0200

    Klasse vergessen

---
+++ ScoreLi/src/tk/scoreli/liveticker/UpdateSpielActivity.java
@@ -0,0 +1,94 @@
+package tk.scoreli.liveticker;
+
+import tk.scoreli.liveticker.data.DatabasehandlerSpiele;
+import tk.scoreli.liveticker.data.Veranstaltung;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UpdateSpielActivity extends Activity {
+	private EditText txfSpielstandHeim, txfSpielstandGast, txfStatus;
+	private Button btnaktualisieren;
+	DatabasehandlerSpiele db = new DatabasehandlerSpiele(this);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_updatespiel);
+		init();
+		btnaktualisieren.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				aktualisieren();
+
+			}
+
+		});
+	}
+
+	private void init() {
+		txfSpielstandHeim = (EditText) findViewById(R.id.txfHeimmannschaftAktualisiere);
+		txfSpielstandGast = (EditText) findViewById(R.id.txfGastmannschaftAktualisiere);
+		txfStatus = (EditText) findViewById(R.id.txfAktualisiereStatus);
+		btnaktualisieren = (Button) findViewById(R.id.btnaktualisieren);
+	}
+
+	private void aktualisieren() {
+		/*
+		 * Hier wird die Zahl(id) der Veranstaltung geholt
+		 */
+		long i = getIntent().getExtras().getLong(SpieleActivity.KEY);
+		Veranstaltung updateveranstaltung = db.getVeranstaltung((int) i);
+		String spielstandheim = txfSpielstandHeim.getText().toString();
+		String spielstandgast = txfSpielstandGast.getText().toString();
+		String spielstatus = txfStatus.getText().toString();
+
+		if (TextUtils.isEmpty(spielstandheim) == false) {
+			updateveranstaltung.setSpielstandHeim(Integer
+					.parseInt(spielstandheim));
+		}
+		if (TextUtils.isEmpty(spielstandgast) == false) {
+			updateveranstaltung.setSpielstandGast(Integer
+					.parseInt(spielstandgast));
+		}
+		if (TextUtils.isEmpty(spielstatus) == false) {
+			updateveranstaltung.setStatus(spielstatus);
+		}
+
+		try {
+			int ka = db.updateVeranstaltung(updateveranstaltung);
+		} catch (Exception e) {
+			Toast.makeText(getApplicationContext(), e.toString(),
+					Toast.LENGTH_LONG).show();
+		}
+
+		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.update_spiel, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
