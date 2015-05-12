7548ae33703170837be0869e6499d11d42937407
Philipp Messmer <philipp.messmer@gmail.com>
2015-05-12 20:41:10 +0200

    Angefangen das Scoreboard zu implementieren mit dem Schalter in der Im Spiel activity

--- ScoreLi/src/tk/scoreli/liveticker/ImSpielActivity.java
+++ ScoreLi/src/tk/scoreli/liveticker/ImSpielActivity.java
@@ -1,17 +1,16 @@
 package tk.scoreli.liveticker;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.Request.Method;
-import com.android.volley.toolbox.StringRequest;
-
+import tk.scoreli.liveticker.bluetooth.BluetoothService;
+import tk.scoreli.liveticker.bluetooth.DeviceListActivity;
 import tk.scoreli.liveticker.data.DatabasehandlerSpiele;
 import tk.scoreli.liveticker.data.DatabasehandlerUUID;
 import tk.scoreli.liveticker.data.Mitglied;
@@ -21,7 +20,12 @@ import tk.scoreli.liveticker.loginregister.AppController;
 import tk.scoreli.liveticker.loginregister.SessionManager;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
@@ -29,25 +33,60 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
-public class ImSpielActivity extends Activity {
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+public class ImSpielActivity extends Activity implements
+		OnCheckedChangeListener {
 	private EditText txfSpielstandHeim, txfSpielstandGast, txfStatus;
 	private Button btnaktualisieren, btnloeschen;
+	private Switch switch_scoreboard;
 	DatabasehandlerSpiele db = new DatabasehandlerSpiele(this);
 	DatabasehandlerUUID dbuuid = new DatabasehandlerUUID(this);
-
+private boolean freigabe = false;
 	private static final String TAG = NeuesSpielActivity.class.getSimpleName();
 	private ProgressDialog pDialog;
 	private SessionManager session;
 	private static final String TAG_VeranstaltungUpdate = "veranstaltungupdate";
+	private static final long serialVersionUID = 1L;
+	// Message types sent from the BluetoothChatService Handler
+	public static final int MESSAGE_STATE_CHANGE = 1;
+	public static final int MESSAGE_READ = 2;
+	public static final int MESSAGE_WRITE = 3;
+	public static final int MESSAGE_DEVICE_NAME = 4;
+	public static final int MESSAGE_TOAST = 5;
+	// Key names received from the BluetoothChatService Handler
+	public static final String DEVICE_NAME = "device_name";
+	public static final String TOAST = "toast";
+
+	// Intent request codes
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+	public static final String KEY = "ID_Veranstaltung";
+	private final static int REQUEST_ENABLE_BT = 2;
+	// Name of the connected device
+	private String mConnectedDeviceName = null;
+	// String buffer for outgoing messages
+	private StringBuffer mOutStringBuffer;
+	// Local Bluetooth adapter
+	private BluetoothAdapter mBluetoothAdapter = null;
+	// Member object for the chat services
+	private BluetoothService mChatService = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_updatespiel);
 		init();
+		switch_scoreboard.setOnCheckedChangeListener(this);
+
 		// Session manager
 		session = new SessionManager(getApplicationContext());
 		// Progress dialog
@@ -59,7 +98,8 @@ public class ImSpielActivity extends Activity {
 				if (session.isLoggedIn()) {
 					aktualisieren();
 				} else {
-					Toast.makeText(getApplicationContext(),
+					Toast.makeText(
+							getApplicationContext(),
 							"Bitte einloggen um Veranstaltungen zu aktualisieren",
 							Toast.LENGTH_SHORT).show();
 				}
@@ -82,6 +122,51 @@ public class ImSpielActivity extends Activity {
 			}
 
 		});
+		/*
+		 * switch_scoreboard.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { Intent serverIntent = new
+		 * Intent(getApplicationContext(), DeviceListActivity.class);
+		 * startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+		 * 
+		 * } })
+		 */
+	}
+
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+		Toast.makeText(this, "The Switch is " + (isChecked ? "on" : "off"),
+				Toast.LENGTH_SHORT).show();
+		if (isChecked) {
+			// Get local Bluetooth adapter
+			mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			// If the adapter is null, then Bluetooth is not supported
+			if (mBluetoothAdapter == null) {
+				Toast.makeText(this, "Bluetooth is not available",
+						Toast.LENGTH_LONG).show();
+				finish();
+				return;
+			}
+			if (!mBluetoothAdapter.isEnabled()) {
+				Intent enableIntent = new Intent(
+						BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+				// Otherwise, setup the chat session
+			} else {
+				if (mChatService == null)
+					setupUebertragung();
+				/**
+				 * Falls beim anmachen des Schalters Bluetooth schon an ist wird
+				 * die Gerätesuche geöffnet.
+				 */
+				Intent serverIntent = new Intent(getApplicationContext(),
+						DeviceListActivity.class);
+				startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+			}
+
+		} else {
+			// do stuff when Switch if OFF
+		}
 	}
 
 	private void init() {
@@ -90,6 +175,8 @@ public class ImSpielActivity extends Activity {
 		txfStatus = (EditText) findViewById(R.id.txfAktualisiereStatus);
 		btnaktualisieren = (Button) findViewById(R.id.btnaktualisieren);
 		btnloeschen = (Button) findViewById(R.id.btnloeschen);
+		switch_scoreboard = (Switch) findViewById(R.id.switch_scoreboard);
+
 	}
 
 	private void loeschen() {
@@ -98,12 +185,14 @@ public class ImSpielActivity extends Activity {
 		 */
 		long i = getIntent().getExtras().getLong(SpieleActivity.KEY);
 		Veranstaltung updateveranstaltung = db.getVeranstaltung((int) i);
-		//db.deleteVeranstaltung(updateveranstaltung); Gleiche wie unten
-		Mitglied uebertrag=dbuuid.getMitglied();
-		Veranstaltungloeschen("" + updateveranstaltung.getId(),uebertrag.getUuid());
-		Toast.makeText(getApplicationContext(), "Gelöscht", Toast.LENGTH_SHORT).show();
+		// db.deleteVeranstaltung(updateveranstaltung); Gleiche wie unten
+		Mitglied uebertrag = dbuuid.getMitglied();
+		Veranstaltungloeschen("" + updateveranstaltung.getId(),
+				uebertrag.getUuid());
+		Toast.makeText(getApplicationContext(), "Gelöscht", Toast.LENGTH_SHORT)
+				.show();
 
-		//finish();
+		// finish();
 	}
 
 	private void aktualisieren() {
@@ -127,14 +216,23 @@ public class ImSpielActivity extends Activity {
 		if (TextUtils.isEmpty(spielstatus) == false) {
 			updateveranstaltung.setStatus(spielstatus);
 		}
-		Mitglied uebertrag=dbuuid.getMitglied();
+		Mitglied uebertrag = dbuuid.getMitglied();
 		try {
-			updateVeranstaltung(""+updateveranstaltung.getSpielstandHeim(),""+ updateveranstaltung.getSpielstandGast(), updateveranstaltung.getStatus(), ""+updateveranstaltung.getId(), uebertrag.getUuid());
-			//db.updateVeranstaltung(updateveranstaltung);Geht nicht irgendwie ist schon geschlossen keine ahnung
+			updateVeranstaltung("" + updateveranstaltung.getSpielstandHeim(),
+					"" + updateveranstaltung.getSpielstandGast(),
+					updateveranstaltung.getStatus(),
+					"" + updateveranstaltung.getId(), uebertrag.getUuid());
+			// db.updateVeranstaltung(updateveranstaltung);Geht nicht irgendwie
+			// ist schon geschlossen keine ahnung
 		} catch (Exception e) {
 			Toast.makeText(getApplicationContext(), e.toString(),
 					Toast.LENGTH_LONG).show();
 		}
+		if(switch_scoreboard.isChecked()&&freigabe
+				){
+			sendVeranstaltung(updateveranstaltung);
+		}
+		
 
 	}
 
@@ -187,12 +285,11 @@ public class ImSpielActivity extends Activity {
 							if (!error) {
 
 								Toast.makeText(getApplicationContext(),
-										"Gelöscht", Toast.LENGTH_SHORT)
-										.show();
+										"Gelöscht", Toast.LENGTH_SHORT).show();
 							} else {
 								Toast.makeText(getApplicationContext(),
-										"Löschen fehlgeschlagen", Toast.LENGTH_SHORT)
-										.show();
+										"Löschen fehlgeschlagen",
+										Toast.LENGTH_SHORT).show();
 							}
 						} catch (JSONException e) { // JSON error
 							e.printStackTrace();
@@ -243,7 +340,7 @@ public class ImSpielActivity extends Activity {
 		// Tag used to cancel the request
 		String tag_string_req = "req_updateveranstaltung";
 
-		pDialog.setMessage("Erstellen ...");
+		pDialog.setMessage("Aktualisieren ...");
 		showDialog();
 
 		StringRequest strReq = new StringRequest(Method.POST,
@@ -261,37 +358,38 @@ public class ImSpielActivity extends Activity {
 								// User successfully stored in MySQL
 								// Now store the user in sqlite
 								// String uid = jObj.getString("uid");
-								/*Toast.makeText(getApplicationContext(),
-										response, Toast.LENGTH_LONG).show();
-								JSONObject user = jObj
-										.getJSONObject("veranstaltung");
-								String idj = user.getString("veranstaltung_id");
-								String sportartj = user.getString("sportart");
-								String heimmannschaftj = user
-										.getString("heimmannschaft");
-								String gastmannschaftj = user
-										.getString("gastmannschaft");
-								String punkteHeimj = user
-										.getString("punkteHeim");
-								String punkteGastj = user
-										.getString("punkteGast");
-								String spielbeginnj = user
-										.getString("spielbeginn");
-								String statusj = user.getString("status");
-							
-								
-
-								int ka = db.updateVeranstaltung(new Veranstaltung(Long
-										.parseLong(idj), sportartj,
-										heimmannschaftj, gastmannschaftj,
-										Integer.parseInt(punkteHeimj), Integer
-												.parseInt(punkteGastj),
-								
-										spielbeginnj, statusj));
-*/
-								//finish();// Hat beendet da man was aufgerufen
-											// hat obwohl es beendet worden ist.
-								Toast.makeText(getApplicationContext(), "Aktualisiert", Toast.LENGTH_SHORT).show();
+								/*
+								 * Toast.makeText(getApplicationContext(),
+								 * response, Toast.LENGTH_LONG).show();
+								 * JSONObject user = jObj
+								 * .getJSONObject("veranstaltung"); String idj =
+								 * user.getString("veranstaltung_id"); String
+								 * sportartj = user.getString("sportart");
+								 * String heimmannschaftj = user
+								 * .getString("heimmannschaft"); String
+								 * gastmannschaftj = user
+								 * .getString("gastmannschaft"); String
+								 * punkteHeimj = user .getString("punkteHeim");
+								 * String punkteGastj = user
+								 * .getString("punkteGast"); String spielbeginnj
+								 * = user .getString("spielbeginn"); String
+								 * statusj = user.getString("status");
+								 * 
+								 * 
+								 * 
+								 * int ka = db.updateVeranstaltung(new
+								 * Veranstaltung(Long .parseLong(idj),
+								 * sportartj, heimmannschaftj, gastmannschaftj,
+								 * Integer.parseInt(punkteHeimj), Integer
+								 * .parseInt(punkteGastj),
+								 * 
+								 * spielbeginnj, statusj));
+								 */
+								// finish();// Hat beendet da man was aufgerufen
+								// hat obwohl es beendet worden ist.
+								Toast.makeText(getApplicationContext(),
+										"Aktualisiert", Toast.LENGTH_SHORT)
+										.show();
 							} else {
 
 								// Error occurred in registration. Get the error
@@ -351,4 +449,226 @@ public class ImSpielActivity extends Activity {
 		if (pDialog.isShowing())
 			pDialog.dismiss();
 	}
+
+	public static byte[] serialize(Object obj) throws IOException {
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		ObjectOutputStream o = new ObjectOutputStream(b);
+		o.writeObject(obj);
+		return b.toByteArray();
+	}
+
+	@Override
+	public synchronized void onPause() {
+		super.onPause();
+		// if(D) Log.e(TAG, "- ON PAUSE -");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		// if(D) Log.e(TAG, "-- ON STOP --");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		// Stop the Bluetooth chat services
+		if (mChatService != null)
+			mChatService.stop();
+		// if(D) Log.e(TAG, "--- ON DESTROY ---");
+	}
+
+	@Override
+	public synchronized void onResume() {
+		super.onResume();
+		// if(D) Log.e(TAG, "+ ON RESUME +");
+
+		// Performing this check in onResume() covers the case in which BT was
+		// not enabled during onStart(), so we were paused to enable it...
+		// onResume() will be called when ACTION_REQUEST_ENABLE activity
+		// returns.
+		/*
+		 * try { Veranstaltungsliste = (ListView)
+		 * findViewById(R.id.listVeranstaltung); ListAdapter listenAdapter = new
+		 * ArrayAdapter<Veranstaltung>(this,
+		 * android.R.layout.simple_list_item_1, db.getAllVeranstaltungen());
+		 * Veranstaltungsliste.setAdapter(listenAdapter);
+		 * Veranstaltungsliste.setOnItemClickListener(this);
+		 * Veranstaltungsliste.setOnItemLongClickListener(this); } catch
+		 * (Exception e) { Toast.makeText(getApplicationContext(), e.toString(),
+		 * Toast.LENGTH_LONG).show();
+		 * 
+		 * }
+		 */
+		if (mChatService != null) {
+			// Only if the state is STATE_NONE, do we know that we haven't
+			// started already
+			if (mChatService.getState() == BluetoothService.STATE_NONE) {
+				// Start the Bluetooth chat services
+				mChatService.start();
+			}
+		}
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// If BT is not on, request that it be enabled.
+		// setupChat() will then be called during onActivityResult
+
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		try {
+			switch (requestCode) {
+			case REQUEST_CONNECT_DEVICE:
+				// When DeviceListActivity returns with a device to connect
+				if (resultCode == Activity.RESULT_OK) {
+					// Get the device MAC address
+					String address = data.getExtras().getString(
+							DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+
+					// Get the BLuetoothDevice object
+					BluetoothDevice device = mBluetoothAdapter
+							.getRemoteDevice(address);
+					// Attempt to connect to the device
+
+					mChatService.connect(device);
+				}
+				break;
+			case REQUEST_ENABLE_BT:
+				// When the request to enable Bluetooth returns
+				if (resultCode == Activity.RESULT_OK) {
+					// Bluetooth is now enabled, so set up a chat session
+					setupUebertragung();
+					Intent serverIntent = new Intent(getApplicationContext(),
+							DeviceListActivity.class);
+					startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+				} else {
+					// User did not enable Bluetooth or an error occured
+					// Toast.makeText(this, R.string.bt_not_enabled_leaving,
+					// Toast.LENGTH_SHORT).show();
+					finish();
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void setupUebertragung() {
+		// Initialize the BluetoothChatService to perform bluetooth connections
+		mChatService = new BluetoothService(this, mHandlerHier);
+		// Initialize the buffer for outgoing messages
+		mOutStringBuffer = new StringBuffer("");
+	}
+
+	private void Bluetoothaktivieren() {
+		boolean bluetoothadapter = true;
+		/**
+		 * Hat das Gerät Bluetooth ?
+		 */
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+				.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(getApplicationContext(),
+					"Bluetooth gerät nicht gefunden", Toast.LENGTH_SHORT)
+					.show();
+			bluetoothadapter = false;
+		}
+		/**
+		 * BluetoothAdapter Aktivieren wenn er nicht schon an ist.
+		 */
+		if (!mBluetoothAdapter.isEnabled() && bluetoothadapter == true) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+
+	}
+
+	private void sendVeranstaltung(Veranstaltung veranstaltung) {
+		// Check that we're actually connected before trying anything
+		if (mChatService.getState() != BluetoothService.STATE_CONNECTED) {
+			// Toast.makeText(this, R.string.not_connected,
+			// Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		// Check that there's actually something to send
+		if (veranstaltung != null) {
+			// Get the message bytes and tell the BluetoothChatService to write
+
+			byte[] send = null;
+			try {
+				send = serialize(veranstaltung);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// send=veranstaltung.getBytes();
+
+			mChatService.write(send);
+
+		}
+	}
+
+	private final Handler mHandlerHier = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case MESSAGE_STATE_CHANGE:
+
+				switch (msg.arg1) {
+				case BluetoothService.STATE_CONNECTED:
+					// mTitle.setText(R.string.title_connected_to);
+					// mTitle.append(mConnectedDeviceName);
+					// mConversationArrayAdapter.clear();
+					break;
+				case BluetoothService.STATE_CONNECTING:
+					// mTitle.setText(R.string.title_connecting);
+					break;
+				case BluetoothService.STATE_LISTEN:
+				case BluetoothService.STATE_NONE:
+					// mTitle.setText(R.string.title_not_connected);
+					break;
+				}
+				break;
+			case MESSAGE_WRITE:
+				byte[] writeBuf = (byte[]) msg.obj;
+				// construct a string from the buffer
+				String writeMessage = new String(writeBuf);
+				// mConversationArrayAdapter.add("Me:  " + writeMessage);
+				break;
+
+			case MESSAGE_DEVICE_NAME:
+				// save the connected device's name
+				mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+				Toast.makeText(getApplicationContext(),
+						"Connected to " + mConnectedDeviceName,
+						Toast.LENGTH_SHORT).show();
+				freigabe=true;
+				break;
+			case MESSAGE_TOAST:
+				Toast.makeText(getApplicationContext(),
+						msg.getData().getString(TOAST),
+
+						Toast.LENGTH_SHORT).show();
+				if (msg.getData().getString(TOAST)
+						.equals("Unable to connect device")
+						|| msg.getData().getString(TOAST)
+								.equals("Device connection was lost")) {
+				switch_scoreboard.setChecked(false);	
+					
+					
+
+				}
+
+				break;
+			}
+		}
+	};
 }
