1e97e3827d1ed47da6c265b8c685e6b9cef9cf48
Philipp Messmer <philipp.messmer@gmail.com>
2015-08-09 22:13:15 +0200

    REgistercode noch hinzugefügt.

--- TTCBeuren/src/de/ttcbeuren/ttcbeurenhauptapp/loginregister/DatabasehandlerBenutzer.java
+++ TTCBeuren/src/de/ttcbeuren/ttcbeurenhauptapp/loginregister/DatabasehandlerBenutzer.java
@@ -66,7 +66,7 @@ public class DatabasehandlerBenutzer extends SQLiteOpenHelper {
 	 * 
 	 * @param mitglied
 	 */
-	public void addBenutzer(Benutzer benutzer) {
+	public void addMitglied(Benutzer benutzer) {
 		SQLiteDatabase db = this.getWritableDatabase();
 
 		ContentValues values = new ContentValues();
